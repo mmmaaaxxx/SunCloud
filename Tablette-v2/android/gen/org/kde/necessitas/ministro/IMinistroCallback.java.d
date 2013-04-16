4e86d568695bf6aed3daa23c95bdcb2929c64298
Maxime Lastera <maxime.lastera@altran.com>
2013-04-16 17:34:45 +0200

    Suppression de Tablette-v2

--- Tablette-v2/android/gen/org/kde/necessitas/ministro/IMinistroCallback.java.d
+++
@@ -1,3 +0,0 @@
-D:\Users\NOMADE\Documents\SunCloud\Tablette_desktop_2\android\gen\org\kde\necessitas\ministro\IMinistroCallback.java: \
-  D:\Users\NOMADE\Documents\SunCloud\Tablette_desktop_2\android\src\org\kde\necessitas\ministro\IMinistroCallback.aidl 
-
